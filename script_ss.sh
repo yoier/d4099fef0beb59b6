6727b50ccd671137f54621b60311d9e5bd546bbe
Yoier <huaimingdeng@gmail.com>
2024-06-09 13:12:24 +0800

    update

--- script_ss.sh
+++ script_ss.sh
@@ -34,11 +34,13 @@ cat >/usr/local/etc/xray/config.json<<EOF
     ]
 }
 EOF
+ufw enable
 ufw allow $port
 ufw reload
+systemctl restart xray.service
 echo "auto update?[y/n]"
 read x1
-if [[ x1 == "y" ]];then
+if [[ $x1 == "y" ]];then
 (crontab -l; echo '0 6 * * 1 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install') | crontab -
 crontab -l
 fi
