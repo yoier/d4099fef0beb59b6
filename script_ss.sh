ad3de7de476c0fafa229767783ace9f072a142ec
Yoier <huaimingdeng@gmail.com>
2024-06-09 12:42:04 +0800

    add ss

---
+++ script_ss.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+apt update
+apt install cron ufw
+echo "input ip"
+read ip
+echo "input port"
+read port
+echo "input psk"
+read psk
+cat >/usr/local/etc/xray/config.json<<EOF
+{
+    "log": null,
+    "inbounds": [
+        {
+            "listen": "$ip",
+            "port": $port,
+            "protocol": "shadowsocks",
+            "settings": {
+                "network": "tcp,udp",
+                "method": "2022-blake3-aes-128-gcm",
+                "password": "$psk"
+            },
+            "streamSettings": {
+                "network": "tcp"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "tag": "direct"
+        }
+    ]
+}
+EOF
+ufw allow $port
+ufw reload
+echo "auto update?[y/n]"
+read x1
+if [[ x1 == "y" ]];then
+(crontab -l; echo '0 6 * * 1 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install') | crontab -
+crontab -l
+fi
